dc3e05507857490140bf2ae9430f151e9364fb2b
Dachialania <alaniadachi25@btu.edu.ge>
2024-06-27 04:34:41 -0700

    commit

---
+++ day 02/homework/lesson 1.py
@@ -0,0 +1,11 @@
+name=" hello,my name is dachi"
+
+last_name="my last_name is alania"
+
+age="and i am 15 years old"
+
+country="i live in georgia"
+
+hobby="my hobby is reading a book"
+
+print(name + " " + last_name + " " + age + " " + country + " " + hobby)
