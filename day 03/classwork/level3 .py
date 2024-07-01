18d7a264105c46494d756154429118faace47473
Dachialania <alaniadachi25@btu.edu.ge>
2024-07-01 04:56:55 -0700

    commit

---
+++ day 03/classwork/level3 .py
@@ -0,0 +1,24 @@
+name="ana"
+score=96
+
+print(name)
+print(score)
+
+
+
+
+#storing the flight number
+flight_n="Bao117"
+
+#storing the flight information
+destination="new york"
+distance=1550
+
+print(flight_n)
+print(destination)
+print(distance)
+
+salary=150000
+pay_rise=10000
+
+print(salary+pay_rise)
