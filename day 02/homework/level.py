ed6097297df0b7a4692f97c0305ceafe5fcff92b
Dachialania <alaniadachi25@btu.edu.ge>
2024-07-03 02:25:45 -0700

    commit

---
+++ day 02/homework/level.py
@@ -0,0 +1,3 @@
+#ვინც გავიცანი გოაში
+
+print("levan samsonidze, nino xvichia, batoni davit janezashvili, elene garibovi, teona oqropilashvili")
---
+++ day 05/homework/level.py
@@ -0,0 +1,34 @@
+number=2
+number1=3
+number2=4
+number3=5
+number4=21
+number5=19
+number6=2
+number7=3
+
+print(number * number1 + number2 - number // number1)
+print(number * number4 + number5 // number7 - number3)
+print(number + number5 * number4 // number3 - number7)
+print(number4 // number6 * number5 - number + number7)
+
+
+name="chemi saxelia dachi"
+last_name="me alania var"
+country="da var sakartvelodan"
+city="konkretulad senakidan"
+
+print(name + ' ' + last_name + ' ' + country + ' ' + city)
+
+mamachemi=47
+dedachemi=40
+chemi_dzma=22
+
+print(mamachemi + 20 , dedachemi + 20 , chemi_dzma + 20 )
+
+
+name="chaweret tkveni saxeli"
+last_name="chaweret tkveni gvari"
+address="chaweret tkveni adgilmdebareoba"
+
+print(name + ' ' + last_name + ' ' + address)
