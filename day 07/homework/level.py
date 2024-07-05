e011c4b9faff4428d1cb814921f6af61f4acd744
Dachialania <alaniadachi25@btu.edu.ge>
2024-07-05 12:09:19 -0700

    commit

---
+++ day 07/homework/level.py
@@ -0,0 +1,21 @@
+for i in range (0,100,2):
+    print(i)
+
+for i in range (0,10):
+    print(i)
+
+
+num=int (input("enter your number"))
+if num<0:
+     print("num is negative")
+elif num>0:
+    print("num is positive")
+
+    
+
+age=int(input("enter your age"))
+if age>18:
+    print("you are adult")
+elif age<18:
+    print("you are virgin")
+
