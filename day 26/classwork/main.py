6d79ddc8de6b8e3f4eea6030d38aedfef4401159
Dachialania <alaniadachi25@btu.edu.ge>
2024-07-08 11:41:52 -0700

    commit

---
+++ day 26/classwork/main.py
@@ -0,0 +1,77 @@
+#name="dachi".capitalize()    #capitalize stringis pirvel asos didad aqcevs
+#print(name)
+
+#name="dachi".upper()      #stringis titoeul asos didad aqcevs
+#print(name)
+
+#   metodi aris chveulebrivi funqcia romelsac iyenebt konkretul monacemta tipze
+
+
+
+
+
+#sentence="my namme is dachi, i from in georgia"
+
+#occurs=sentence.count("dachi")
+                     
+
+#print(occurs)
+
+
+
+
+
+
+
+
+
+
+
+#name="dachi".upper()
+#print(name)
+
+#name="DACHI".lower()
+#print(name)
+
+
+#count მეთოდი ითვლის, რამდენჯერ მოიძებნება  არსებული სტრინგში მოცემული სიმბოლოები.
+#lower სიტყვებს აპატარვებს.
+#სტრინგი არის ტიპის მონაცემთა ტიპი, რომელიც შედგება სიმბოლოების მიმართ 
+
+
+
+#sentence="my namme is DACHI, i from in georgia"
+
+#index= sentence.find("DACHI")
+
+#print(index)
+
+
+#sentence="my namme is dachi, i from in georgia"
+
+#index=sentence.index("dachi")
+
+#print(index)
+#find მეთოდი აბრუნებს ინდექსს  პირველიდან დაწყებულ სიმბოლოს/სიმბოლოების მოძებნას სტრინგში.
+
+#sentence="my namme is dachi, i from in georgia"
+#print(sentence.startswith("my"))
+#chven shegvidzlia shevamwomot iwyeba tu ara magalitad my sityvit da es ikneba swori da daushvat i mashin ikneba false
+#sentence="my namme is dachi, i from in georgia"
+#print(sentence.endswith("georgia"))
+#da endswith tu riti mtavrdeba winadadeba.
+
+
+#num_emails = int(input("რამდენი იმეილის შემოტანა გსურთ? "))
+#emails = [5]
+
+#for _ in range(num_emails):
+    #email = input("გთხოვთ, შეიყვანეთ იმეილი: ")
+    #emails.append(email)
+
+
+#for email in emails:
+    #if is_valid_email(email):
+       # print(f"{email} - სწორი")
+   # else:
+       # print(f"{email} - არასწორი")
